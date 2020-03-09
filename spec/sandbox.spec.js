edf505186550e813b4c8a17c2e182203fdcc0626
Girma Nigusse <xgirma@gmail.com>
2020-03-08 23:37:44 -0400

    update,update test url

--- spec/sandbox.spec.js
+++ spec/sandbox.spec.js
@@ -2,7 +2,7 @@ import { strictEqual } from "assert";
 
 describe("Sandbox", () => {
   beforeAll(() => {
-    browser.get("https://e2e-boilerplates.github.io/sandbox/");
+    browser.get("https://e2e-boilerplate.github.io/sandbox/");
   });
 
   it("should be on Sandbox", async () => {
